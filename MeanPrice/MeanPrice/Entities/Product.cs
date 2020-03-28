17f8d518ad2fb7522598e7ef4fbe74494385f06d
wagnerlinhares <wagnerlink@gmail.com>
2020-03-28 18:15:57 -0300

    Initial Commit

---
+++ MeanPrice/MeanPrice/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeanPrice.Entities
+{
+    class Product
+    {
+        public String Name { get; set; }
+
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
